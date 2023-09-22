a3f8cc405993bab5663343d5416a7ffcda0dd839
gustave mary <gustave1mary@gmail.com>
2023-09-22 09:22:13 +0200

    end of the project

--- exercise14/main.py
+++
@@ -1,8 +0,0 @@
-import sys
-sys.path.append('../exercise00/')
-from vector import Vector
-from matrix import Matrix
-sys.path.append('../utils/')
-from utils import green, blue, yellow
-
-if __name__ == "__main__":
